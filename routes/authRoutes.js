76b5e39f3d329d252e9e21f57c8a7ec6df33ec8c
Alessio C <alessiococi1986@gmail.com>
2026-06-24 15:34:38 +0100

    Add user registration with hashed passwords

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import { pool } from "../db/index.js"
 
 const router = express.Router();
 
@@ -9,5 +11,31 @@ router.get("/register", (req, res) => {
 router.get("/login", (req, res) => {
     res.send("Login will come soon")
 });
+
+router.post("/register", async(req, res) => {
+
+    try {
+        const { name, email, password} = req.body;
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        await pool.query (
+            "INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3)",
+            [name, email, passwordHash]
+            
+        );
+        res.redirect("/auth/login")
+    }
+
+    catch(error) {
+        console.error(error);
+        res.status(500).send("Something went wrong!")
+    }
+
+})
+
+
+
+
+
 
 export default router 
