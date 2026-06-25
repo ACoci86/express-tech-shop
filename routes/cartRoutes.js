dac8888c43fb71639a42e1a861b2c31736792fdb
Alessio C <alessiococi1986@gmail.com>
2026-06-25 11:08:34 +0100

    Display cart items

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,8 +4,54 @@ import { requireAuth } from "../middleware/requireAuth.js";
 
 const router = express.Router();
 
+router.post("/add/:productId", requireAuth, async (req, res) => {
+  try {
+    const userId = req.session.userId;
+    const productId = req.params.productId;
+
+    await pool.query(
+      `
+      INSERT INTO cart_items (user_id, product_id, quantity)
+      VALUES ($1, $2, 1)
+      ON CONFLICT (user_id, product_id)
+      DO UPDATE SET quantity = cart_items.quantity + 1
+      `,
+      [userId, productId]
+    );
+
+    res.redirect("/cart");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong adding item to cart");
+  }
+});
+
 router.get("/", requireAuth, async (req, res) => {
-  res.send("Cart page coming soon");
+  try {
+    const userId = req.session.userId;
+
+    const result = await pool.query(
+      `
+      SELECT 
+        cart_items.id AS cart_item_id,
+        cart_items.quantity,
+        products.id AS product_id,
+        products.name,
+        products.price,
+        products.image_url
+      FROM cart_items
+      JOIN products ON cart_items.product_id = products.id
+      WHERE cart_items.user_id = $1
+      ORDER BY cart_items.id ASC
+      `,
+      [userId]
+    );
+
+    res.render("pages/cart", {  cartItems: result.rows})
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Something went wrong loading your cart");
+  }
 });
 
 export default router;
